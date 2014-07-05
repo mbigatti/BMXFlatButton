6460122cb805eabd429326d27c9ec5afe25b78aa
mbigatti <massimiliano@bigatti.it>
2014-07-05 20:26:59 +0200

    Initial Commit

---
+++ BMXFlatButton/BMXFlatButton.h
@@ -0,0 +1,19 @@
+//
+//  BMXFlatButton.h
+//  BMXFlatButton
+//
+//  Created by Massimiliano Bigatti on 05/07/14.
+//  Copyright (c) 2014 Massimiliano Bigatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMXFlatButton.
+FOUNDATION_EXPORT double BMXFlatButtonVersionNumber;
+
+//! Project version string for BMXFlatButton.
+FOUNDATION_EXPORT const unsigned char BMXFlatButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMXFlatButton/PublicHeader.h>
+
+
