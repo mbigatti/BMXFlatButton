30764df74ee625f11331875f7988cb72673f5483
mbigatti <massimiliano@bigatti.it>
2014-07-07 10:12:48 +0200

    added CircularButton, refactoring

---
+++ BMXFlatButton/Buttons.swift
@@ -0,0 +1,112 @@
+//
+//  AbstractButton.swift
+//  BMXFlatButton
+//
+//  Created by Massimiliano Bigatti on 06/07/14.
+//  Copyright (c) 2014 Massimiliano Bigatti. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class AbstractButton: UIButton {
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @IBInspectable var normalColor : UIColor = UIColor.clearColor() {
+    didSet {
+        updateButtonColor()
+    }
+    }
+    
+    @IBInspectable var highlightedColor : UIColor? {
+    didSet {
+        updateButtonColor()
+    }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+    didSet {
+        layer.borderWidth = borderWidth
+    }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+    didSet {
+        layer.borderColor = borderColor.CGColor
+    }
+    }
+    
+    override var highlighted : Bool {
+    didSet {
+        updateButtonColor()
+    }
+    }
+    
+    func updateButtonColor() {
+        if highlighted {
+            if highlightedColor {
+                self.backgroundColor = highlightedColor!
+            } else {
+                self.backgroundColor = normalColor.darkerColor(0.2)
+            }
+        } else {
+            self.backgroundColor = normalColor
+        }
+    }
+}
+
+@IBDesignable
+class FlatButton: AbstractButton {
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @IBInspectable override var normalColor : UIColor { didSet{} }
+    @IBInspectable override var highlightedColor : UIColor? { didSet{} }
+    @IBInspectable override var borderWidth : CGFloat { didSet{} }
+    @IBInspectable override var borderColor : UIColor { didSet{} }
+    
+    @IBInspectable var cornerRadius : CGFloat = 2 {
+    didSet {
+        self.layer.cornerRadius = cornerRadius
+    }
+    }
+    
+}
+
+@IBDesignable
+class CircularButton: AbstractButton {
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @IBInspectable override var normalColor : UIColor { didSet{} }
+    @IBInspectable override var highlightedColor : UIColor? { didSet{} }
+    @IBInspectable override var borderWidth : CGFloat { didSet{} }
+    @IBInspectable override var borderColor : UIColor { didSet{} }
+    
+    override var frame: CGRect {
+    didSet {
+        let wide = max(frame.size.width, frame.size.height)
+        layer.cornerRadius = wide / 2
+    }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,26 +1,33 @@
 # BMXFlatButton
 
-A custom Swift `UIButton` with a flat appearance, released as a Cocoa Touch Framework. It supports:
+A couple of Swift `UIButton` with a flat appearance, released as a Cocoa Touch Framework. The framework contains two button types: `FlatButton` and `CircularButton`. The first is a squared button with optional rounded corners and border; you can tweak the colors to obtain a filled or outlined style. The circular button is a circle shaped button intended to be used without a title; it also can have a optional border.
+
+## Features
 
 - designable in Interface Builder
-- customizable corner radius
 - customizable highlighted color (defaults to normal color but 2% darker)
+- customizable border size and color
+- customizable corner radius (only for `FlatButton`)
 
-![Button example](http://f.cl.ly/items/2Q1d2E1f1U2H2e0B3M1m/Schermata%202014-07-05%20alle%2021.41.36.png)
+![Button example](http://f.cl.ly/items/0i1l2d1o2e1E0r323l2D/BMXFlatButtonExample.png)
 
 For a complete example see the enclosed `BMXFlatButtonTest`project.
 
 ## Usage
 
-- Add `BMXFlatButton` framework to project in Project > General > Linked Frameworks and Libraries.
-- For each button that you want to be rendered with `BMXFlatButton`, set Custom Class to FlatButton in Identity Inspector.
+- Add `BMXFlatButton` framework to project in `Project > General > Linked Frameworks and Libraries`.
+- Set *Custom Class* in *Identity Inspector*. Choose `FlatButton` for flat squared buttons or `CircularButton` for circular buttons.
 
 ![Identity Inspector](http://f.cl.ly/items/381e1A1g0U2D08420s1q/Schermata%202014-07-05%20alle%2021.49.25.png)
 
-- configure the Flat Button properties as required.
+- configure the button properties as required in *Attribute Inspector*.
 
-![Attribute Inspector](http://f.cl.ly/items/2B102T1g0s091T1E2I2S/Schermata%202014-07-05%20alle%2021.50.33.png)
+![Flat Button - Attribute Inspector](http://f.cl.ly/items/1E0D1r1g02351Y0m1s32/IB1.png)
 
+![Circular Button - Attribute Inspector](http://f.cl.ly/items/213t293C3R2L1W2I0d42/IB2.png)
+
+## Notes
+Due to a current Interface Builder limitation, the Custom Class Class field does not show the `FlatButton` and `CircularButton` options in the selection box, you'll need to write them manually.
 
 ### Contact
 [http://bigatti.it](http://bigatti.it)  
